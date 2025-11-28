4f5b4d273f36805afb259f1524a59abcf764ae85
jonlindholm11 <jonlindholm11@gmail.com>
2025-11-28 12:47:16 -0500

    fixed app py to include all the api routers

--- backend/db/app.py
+++ backend/db/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import user_routes
+import system_routes
+import game_routes
+import franchise_routes
+import emulator_routes
 import ping_routes
 
 app = FastAPI()
 app.include_router(user_routes.router)
+app.include_router(system_routes.router)
+app.include_router(game_routes.router)
+app.include_router(franchise_routes.router)
+app.include_router(emulator_routes.router)
 app.include_router(ping_routes.router)
